154d52101d58a463441c850376d3005632562555
Abhishek <akthakuron120@gmail.com>
2023-03-26 16:18:03 +0530

    Liner search function added

--- hell.cpp
+++ hell.cpp
@@ -10,6 +10,13 @@ bool isEven(int n) {
     else return false;
 }
 
+int linerSearch(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) return i;
+    }
+    return -1;
+}
+
 int main() {
     cout << "Hello from Hell!" << endl;
     return 0;
