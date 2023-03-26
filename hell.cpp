e889daefa4e5b3899a4f06f2e589e6d5ad10cb5d
me-hariom7 <hariom15092002@gmail.com>
2023-03-26 16:11:32 +0530

    added isEven function

--- hell.cpp
+++ hell.cpp
@@ -5,6 +5,11 @@ int fib(int n) {
     return fib(n-1) + fib(n-2);
 }
 
+bool isEven(int n) {
+    if(n % 2 == 0) return true;
+    else return false;
+}
+
 int main() {
     cout << "Hello from Hell!" << endl;
     return 0;
